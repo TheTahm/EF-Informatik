ead523ee1d3e2cc5c4859b17bb9a86ddb5cc7ea5
Noah <noah.scherler@edu.gbsl.ch>
2022-09-09 15:20:40 +0200

    verbe

--- exercises/primzahlen.py
+++ exercises/primzahlen.py
@@ -5,7 +5,7 @@ for i in range(start, end+1):
   if i>1: 
     for j in range(2,i): 
         if(i % j==0): 
-            break
+           break 
     else: 
         prime_zahl.append(i)
 print(prime_zahl)
