1dc1d6fc77e3b9810812cc1bde47b5def965186c
Noah <noah.scherler@edu.gbsl.ch>
2022-09-16 16:37:38 +0200

    Frustration

---
+++ numtrip/game.py
@@ -0,0 +1,12 @@
+[16:36] Arnaschus Linus, GBSL Schueler
+zeilen = 10
+matrix = [
+  [2,3,2,6,5],[6,2,5,7,4],[3,9,5,2,0],[0,6,1,3,4],[2,4,6,8,2]
+]
+def pprint(mat2d):
+     for zeile in mat2d:
+     print(zeile)
+
+pprint (matrix)
+
+print("+-----+------+------+-----+-----+-----+\n"  "I    I     I      I    I    I     I")
