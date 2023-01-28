ca6a96afdd827b18ab920e6719a7264d917401c9
Noah <noah.scherler@edu.gbsl.ch>
2023-01-28 02:28:26 +0100

    Korrigiert Schreibfehler in Komentaren

--- docs/numtrip/game.py
+++ docs/numtrip/game.py
@@ -5,7 +5,7 @@ board = []
 
 def board_auffüllen():
     global board
-    board = [     # Füllt das Board mit zufälligen Werten.Werte=(2,4,8)  
+    board = [     # Füllt das Board mit zufälligen Werten  Werte=(2,4,8)  
         [random.choice(numbers),random.choice(numbers), random.choice(numbers), random.choice(numbers), random.choice(numbers)],
         [random.choice(numbers),random.choice(numbers), random.choice(numbers), random.choice(numbers), random.choice(numbers)],
         [random.choice(numbers),random.choice(numbers), random.choice(numbers), random.choice(numbers), random.choice(numbers)],
@@ -20,7 +20,7 @@ def spielfeld():    # Druckt das Spielfeld aus
     print (' ', end='')
     for i in board:
         x = x+1
-        print ('  ', x, end='') # printet die Zahlen 1-5 obe
+        print ('  ', x, end='') # printet die Zahlen 1-5 oben
     print (' ')
     print ('  ', end='')
     #Zahlen seite = y
@@ -46,16 +46,16 @@ def spielfeld():    # Druckt das Spielfeld aus
 
 def Süberprüfen(frage): # überprüft die Spaltenauswahl
     valid=True
-    while valid:        # Widerholt es bis eine richtige Eingabe geschiet
+    while valid:        # Wiederholt es bis eine richtige Eingabe geschiet
         try:
             zahl=input(frage)
             zahl = int(zahl)
-            zahl = zahl -1          #Muss -1 grechnet werden, damit man 1-5 eingeben kann und nicht 0-4
-            if zahl<0 or zahl > 4:   #Bei grösser als 4 Zählt 4 auch dazu
+            zahl = zahl -1          # Muss -1 grechnet werden, damit man 1-5 eingeben kann und nicht 0-4
+            if zahl<0 or zahl > 4:   # Bei grösser als 4 Zählt 4 auch dazu
                 raise
-            return zahl     #Gibt die Zahl zurück und stoppt die Überprüfung
+            return zahl     # Gibt die Zahl zurück und stoppt die Überprüfung
         except:
-            print ('Fehlerhafte Eingabe')      #Es wird automatisch wiederholt
+            print ('Fehlerhafte Eingabe')      # Es wird automatisch wiederholt
 
 def Züberprüfen(frage): # überprüft die Zeilenauswahl
     valid=True
@@ -64,7 +64,7 @@ def Züberprüfen(frage): # überprüft die Zeilenauswahl
             zahl=input(frage)
             zahl = int(zahl)
             zahl = zahl -1
-            if zahl<0 or zahl > 4: # Bei kleiner als 0 zählz 0 nicht dazu
+            if zahl<0 or zahl > 4: # Bei kleiner als 0 zählt 0 nicht dazu
                 raise 
             return zahl
         except:
@@ -74,7 +74,7 @@ def flood_fill(x ,y, old, new): # leert alle anliegenden Felder mit dem Glecihen
     if x < 0 or x >= len(board[0]) or y < 0 or y >= len(board): # schaut ob das Feld noch im Spielfeld ist
         return      # falls nicht, wird es abgebrochen
     if board[x][y] != old:      # schaut ob die Zahl des ausgewählten Felds nicht mit dem Feld übereinstimmt.
-        return
+        return      # falls er nicht übereinstimmt, wird der Code abgebrochen
     board[x][y] = new   # Ersetzt das Feld wenn es den Wert wenn er Gleich ist wie das originale hat zu ' '
     flood_fill(x+1, y, old, new) # Wiederholt die Funktion für ein Feld weiter unten
     flood_fill(x-1, y, old, new) # darüber
@@ -105,18 +105,18 @@ def Verdoppelung(Feld): # Verdoppelt den Originalwert
 def Auffüllen():    # Verschiebt die leeren Zellen nach unten und füllt leere Felder am Schluss wieder auf
     y=0
     x=4
-    for i in range(25): # man muss dies 25 Mal auffüllen, damit jedes Feld angeschaut wird
+    for i in range(25): # man muss dies 25 Mal ausführen, damit jedes Feld angeschaut wird
         if y>=5:
             y=0
         Zeilen=4
-        for i in range (4): # dies nur vier Mal, denn sonst würde man die oberste zeile mit einer Zeile darüber vergleichen wo wir nichts haben
+        for i in range (4): # dies nur vier Mal, denn sonst würde man die oberste Zeile mit einer Zeile darüber vergleichen wo wir nichts haben
             if (board[x][y])==' ': # überprüft ob das jetzige Feld leer ist, falls ja kopiert es den Wert vom obigen
                 x=x-1
                 board[Zeilen][y]=board[x][y]
-                board[x][y]=' ' #das obige Feld wird leer gesetzt
+                board[x][y]=' ' # das obige Feld wird leer gesetzt
             Zeilen=Zeilen-1
             x=Zeilen
-        if board[x][y]==' ':   #falls es am schluss noch leere Felder gibt, werden diese mit zufälligen Zahlen aufgefüllt
+        if board[x][y]==' ':   # falls es am schluss noch leere Felder gibt, werden diese mit zufälligen Zahlen aufgefüllt
             board[x][y] = random.choice(numbers)
         y=y+1
         x=4
@@ -135,7 +135,7 @@ def lose():
     a=0
     for y in range(5): # y ist die Zeilenzahl
         for x in range(5): # x ist die Spaltenzahl
-            z=0         #z muss bei jedem neuen Feld 0 gesetzt werden
+            z=0         # z muss bei jedem neuen Feld 0 gesetzt werden
             if x!=4:
                 if board[x+1][y]!=board[x][y]:     #Überprüft das Feld darunter
                     z=z+1                           
@@ -159,15 +159,15 @@ def lose():
             if z==4:
                 a=a+1
     if a==25:
-        print (f'Sie haben in {o} Zügen veloren!') # printet die Anzahl Spielzüge aus
+        print (f'Sie haben in {o} Zügen veloren!') # printet die Anzahl Spielzüge
         o=0     # resettet den Spielzugzähler
-        Wiederspielen("Wollen Sie noch einmal spielen (ja/nein)?") #Nachdem man verloren hat kann man eine neue Runde anfangen oder aufhören
+        Wiederspielen("Wollen Sie noch einmal spielen (ja/nein)?") # Nachdem man verloren hat, kann man eine neue Runde anfangen oder aufhören
 
 def Wiederspielen(frage):   # Ermöglicht es eine neue Runde anzufangen oder aufzuhören 
     loop=input(frage)       # fragt ob man weiterspielen will
-    loop=loop.lower()       # ermöglicht es die antwort gross zu schreiben, und es wird trozdem gezählt
-    loop=loop.strip()       # entfernt mögliche leerzeichen etc. vor und nam dem string
-    if loop=="ja":      # falls man weiterspielen will, wird das board neu zufällig ausgefüllt und angezeigt
+    loop=loop.lower()       # ermöglicht es die Antwort gross zu schreiben, und es wird trozdem gezählt
+    loop=loop.strip()       # entfernt mögliche Leerzeichen etc. vor und nach dem String
+    if loop=="ja":      # falls man weiterspielen will, wird das Board neu zufällig ausgefüllt und angezeigt
         board_auffüllen()
         spielfeld()
     elif loop=="nein": # falls nicht, hört der ganze code auf
@@ -176,7 +176,7 @@ def Wiederspielen(frage):   # Ermöglicht es eine neue Runde anzufangen oder auf
         print("Falsche Eingabe")
         Wiederspielen("Wollen Sie noch einmal spielen (ja/nein)?")
 
-def Spielen(frage): # ist dazu da um zu entscheiden ob man spielen will, funktioniert gleich wie Wiederspielen, einfach dass die druchgegebene Frage anders ist
+def Spielen(frage): # ist dazu da um zu entscheiden ob man spielen will, funktioniert gleich wie Wiederspielen, einfach dass die durchgegebene Frage anders ist
     loop=input(frage)
     loop=loop.lower()
     loop=loop.strip()
@@ -194,8 +194,8 @@ not_game_over=True  # solange, dass dies True ist, wird das Spiel weitergespielt
 o=0 # definiert den Spielzugzähler
 while not_game_over:
     o=o+1 # der Spielzugzähler, wird nach jedem zyklus +1 gerechnet 
-    print (f'Spielzug {o}') # Gibt die Anzahl $spielzüge an, die man schon gepsielt hat
-    Zauswahl=Züberprüfen("Welche Zeile 1-5?") # Zauswahl, ist überprüfte Zahl der eingabe des Benutzers für die Zeile
+    print (f'Spielzug {o}') # Gibt die Anzahl Spielzüge an, die man schon gepsielt hat
+    Zauswahl=Züberprüfen("Welche Zeile 1-5?") # Zauswahl, ist die überprüfte Zahl der eingabe des Benutzers für die Zeile
     Sauswahl=Süberprüfen("Welche Spalte 1-5?") # Sauswahl, für die Spalten
     Wert=board[Zauswahl][Sauswahl] # kopiert den Wert des ausgewählten Feldes, befor es gelöscht wird
     flood_fill(Zauswahl,Sauswahl,board[Zauswahl][Sauswahl],' ') # leert anligende Felder mit dem gleichen Wert
